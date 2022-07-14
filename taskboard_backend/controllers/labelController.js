aaddd80c04bf73032c892700b0b4f14b438ce0ee
Abdelouahed Erraji <abdelouahederraji1@gmail.com>
2022-07-14 13:32:10 +0100

    - add label update in backend

--- taskboard_backend/controllers/labelController.js
+++ taskboard_backend/controllers/labelController.js
@@ -1,30 +1,36 @@
-const Label = require("../models/label");
+const Label = require('../models/label');
 
-exports.createLabel = async (req, res) => {
+exports.getLabelsOfBoard = async (req, res) => {
   try {
-    console.log(req.body);
-    const label = await Label.create(req.body);
+    const label = await Label.find({ board: req.params.board });
     res.send(label);
   } catch (err) {
     console.log(err);
   }
 }
 
-exports.getLabelOfCard = async (req, res) => {
+exports.createLabel = async (req, res) => {
   try {
-    console.log('request', req);
-    const label = await Label.find({card: req.params.cardId}, "name color");
+    console.log('body', req.body);
+    const label = Label(req.body);
+    await label.save();
     res.send(label);
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 }
 
 exports.updateLabel = async (req, res) => {
   try {
-    const response = await Label.updateOne({card: req.params.cardId}, {color: req.body.color, name: req.body.name});
-    res.send(response);
-  } catch(err) {
+    const labelToUpdate = await Label.findById(req.params.id);
+    if(labelToUpdate.cards.includes(req.body.card)) return;
+    const updatedLabels = await Label.find({cards: [req.body.card]});
+    for(let i = 0; i < updatedLabels.length; i++) {
+      await Label.updateOne({_id: updatedLabels[i]._id}, {cards: updatedLabels[i].cards.filter((c) => c._id.toString() !== req.body.card)});
+    }
+    await Label.findByIdAndUpdate(labelToUpdate._id, { cards: [...labelToUpdate.cards, req.body.card] });
+    res.send(updatedLabels);
+  } catch (err) {
     console.log(err);
   }
 }
