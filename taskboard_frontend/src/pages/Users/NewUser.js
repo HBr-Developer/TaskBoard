d4cd3359f5ee5c4b7aeb4e059124b19ea619bcb1
Abdelouahed Erraji <abdelouahederraji1@gmail.com>
2022-07-17 23:20:32 +0100

    - add newuser for admin(by hanane)

---
+++ taskboard_frontend/src/pages/Users/NewUser.js
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import axios from "axios";
+import { Alert } from "@mui/material";
+
+const NewUser = ({ setUsers, users, userToUpdate, setOpenPopup }) => {
+  const [formData, setFormData] = useState({
+    name: userToUpdate ? userToUpdate.name : '',
+    email: userToUpdate ? userToUpdate.email : '',
+    password: '',
+    password2: ''
+  });
+  
+  const { name, email, password, password2 } = formData;
+  
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    // add user
+    if (password !== password2) {
+      console.log("Passwords don't match");
+    } else {
+      if (!userToUpdate) {
+        const userData = {
+          name,
+          email,
+          password
+        }
+        const newUser = await axios.post("http://localhost:3001/member/", userData);
+        setUsers([...users, newUser.data]);
+        setOpenPopup(false);
+      }
+    }
+    
+    // update user
+    if (userToUpdate) {
+      try {
+        await axios.patch(`http://localhost:3001/member/${userToUpdate._id}`, {
+          name,
+          email,
+          currentPassword: password,
+          newPassword: password2
+        });
+        const updatedUserIndex = users.indexOf(userToUpdate);
+        users.splice(updatedUserIndex, 1, { ...users[updatedUserIndex], name: formData.name, email: formData.email });
+        setUsers(users);
+        setOpenPopup(false);
+      } catch (err) {
+        <Alert severity="success" color="info">
+          {err.response.data}
+        </Alert>
+      }
+    }
+  }
+  //
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+  
+  return (
+    <div className="form" style={{ width: '400px', padding: "20px 10px" }}>
+      <form onSubmit={handleOnSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input style={{ padding: '5px 6px', fontSize: '1rem' }} type="text" id="name" name="name" value={name}
+                 pattern="^\w+( \w+)$"
+                 title="first name space last name" onChange={onChange} required/>
+        </div>
+        <div className="input-container">
+          <label htmlFor='email'>Email </label>
+          <input style={{ padding: '5px 6px', fontSize: '1rem' }} type="email" id="email" name="email" value={email}
+                 onChange={onChange} required/>
+        </div>
+        {!userToUpdate ? (
+          <>
+            <div className="input-container">
+              <label>Password </label>
+              <input style={{ padding: '5px 6px', fontSize: '1rem' }} type="password" id="password" name="password"
+                     value={password} onChange={onChange} required/>
+            </div>
+            <div className="input-container">
+              <label>Confirm password </label>
+              <input style={{ padding: '5px 6px', fontSize: '1rem' }} type="password" id="password2" name="password2"
+                     value={password2} onChange={onChange} required/>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="input-container">
+              <label>Current Password </label>
+              <input style={{ padding: '5px 6px', fontSize: '1rem' }} type="password" id="password" name="password"
+                     value={password} onChange={onChange} required/>
+            </div>
+            <div className="input-container">
+              <label>New password </label>
+              <input style={{ padding: '5px 6px', fontSize: '1rem' }} type="password" id="password2" name="password2"
+                     value={password2} onChange={onChange}/>
+            </div>
+          </>
+        )}
+        <div className="button-container">
+          <input style={{ borderRadius: 3 }} type="submit" value={userToUpdate ? "Save Changes" : "Save User"}/>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default NewUser;
