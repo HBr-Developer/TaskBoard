0b04c1a2c33bd7ec43dd7be8dfe4aba2cfc36290
Abdelouahed Erraji <abdelouahederraji1@gmail.com>
2022-07-17 23:17:37 +0100

    - add admin sidebar (by hanane)

---
+++ taskboard_frontend/src/components/Sidebar/AdminSidebar.js
@@ -0,0 +1,37 @@
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { Link } from 'react-router-dom'
+import './sidebar.css';
+
+function Sidebar({ showSidebar, setShowSideBar }) {
+  const hideSideBar = () => {
+    setShowSideBar(!showSidebar);
+  }
+  
+  return (
+    <>
+      <nav className={showSidebar ? 'sidebar active' : 'sidebar'}>
+        <ul className='side-menu-items'>
+          <Link to='#' className='menu-bars'>
+            <div className='direction'>
+              <ChevronLeftIcon className='chevronLeft' onClick={hideSideBar}/>
+              <ChevronRightIcon className={showSidebar ? 'chevronRight' : ''} onClick={hideSideBar}/>
+            </div>
+          </Link>
+          <li className='side-text'>
+            <Link to='/board'>
+              <span>Projects</span>
+            </Link>
+          </li>
+          <li className='side-text'>
+            <Link to='/users'>
+              <span>Users</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Sidebar;
